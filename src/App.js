bd4ca9c0522ec0e9b713253ed58b6665c4b7a372
Sebastian Garcia <121516129+SebCodes27@users.noreply.github.com>
2023-07-01 00:29:47 -0700

    Final changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class Calculator extends React.Component {
       text: '',
       operator: '',
       newInt: '',
-      prevTotal: 0,
+      prevTotal: '',
       firstOp: 'no',
       totaled: 'no'
     }
@@ -23,9 +23,27 @@ class Calculator extends React.Component {
     document.addEventListener("keydown", this.handleKeyPress); 
   }
   handleKeyPress(event) { // handles key inputs, including 'enter', 'escape', and operations and numbers which are identified by id
-    let clicked = document.getElementById(event.key);
+    let numObj = {
+      0: 'zero',
+      1: 'one',
+      2: 'two',
+      3: 'three',
+      4: 'four',
+      5: 'five',
+      6: 'six',
+      7: 'seven',
+      8: 'eight',
+      9: 'nine',
+      '+': 'add',
+      '-': 'subtract',
+      'x': 'multiply',
+      '/': 'divide',
+      '.': 'decimal'
+    }
+    let key = numObj[event.key];
+    let clicked = document.getElementById(key);
     if (event.key === 'Enter') {
-      let clicked = document.getElementById('total');
+      let clicked = document.getElementById('equals');
       clicked.click();
     }
     if (event.key === 'Escape') {
@@ -43,8 +61,28 @@ class Calculator extends React.Component {
     if (this.state.totaled === 'yes') { // prevents new values being concatenated to a total
       return;
     }
+    if (num === '0') {
+      let truthVal = 1;
+      let length = this.state.text.length;
+      for (let i = length; i > 0; i--) {
+        let textLen = this.state.text[i - 1]
+        if (textLen === '0') {
+          truthVal *= 0;
+        } else if (/[1-9]|./g.test(textLen) === true) {
+          truthVal += 1;
+          i = 0;
+        }
+        else if (textLen === '+' || textLen === '-' || textLen === 'x' || textLen === '/' ) {
+          i = 0
+        }
+      }
+      console.log(truthVal);
+      if (truthVal === 0) {
+        return;
+      }
+    }
     let newText = this.state.text + num; // prepares the window preview
-    let newVal = 0;
+    let newVal = '';
     let lastVal = this.state.text[this.state.text.length - 1];
     let strPrevTotal = this.state.prevTotal.toString() + num;
     if (this.state.firstOp === 'no') { // handles the inclusion of decimals in the first input value, i.e '1.05 x 3'
@@ -58,7 +96,7 @@ class Calculator extends React.Component {
         })
     }
   }
-    if (lastVal !== '+' && lastVal !== '-' && lastVal !== 'x' && lastVal !== '÷') { // concatenates, rather than operates, new values, '3' + '3' = 33
+    if (lastVal !== '+' && lastVal !== 'x' && lastVal !== '÷') { // concatenates, rather than operates, new values, '3' + '3' = 33
       newVal += (this.state.newInt).toString() + num;
     } else {
       newVal += num;
@@ -77,7 +115,7 @@ class Calculator extends React.Component {
       return;
     }
     let newText = this.state.text + dec; // prepares the window preview
-    let newVal = 0;
+    let newVal = '';
     let lastVal = this.state.text[this.state.text.length - 1];
     let strPrevTotal = this.state.prevTotal.toString() + dec;
     if (this.state.firstOp === 'no') { // handles the inclusion of decimals in the first input value, i.e '1.05 x 3'
@@ -99,22 +137,29 @@ class Calculator extends React.Component {
   }
   handleOpChange(sym, operator) { // handles operations
     let lastVal = this.state.text[this.state.text.length - 1];
-    if (lastVal === '+' || lastVal === "-" || lastVal === 'x' || lastVal === '÷') { // prevents double use of operations
-      return
-    }
     let newText = this.state.text + sym; // sets up the preview
     let currOp = this.state.operator;
+    let operatorVal = operator;
+    let newVal = '';
+    if (lastVal === '+' || lastVal === "-" || lastVal === 'x' || lastVal === '÷') { // prevents double use of operations
+      currOp = '';
+      if (operator === 'minus') {
+        operatorVal = this.state.operator;
+        newVal = '-' + (this.state.newInt).toString();
+      }
+    }
     if (this.state.totaled === 'yes') {
       currOp = ''
     }
     this.setState({ // sets window preview, operator value, true or false of first operation, and true or false of a previous total existing
       text: newText,
-      operator: operator,
+      operator: operatorVal,
       firstOp: 'yes',
-      totaled: 'no'
+      totaled: 'no',
+      newInt: newVal, /* the final melon */
     });
-    let prevTotal = 0;
-    let length = 0;
+    let prevTotal = '';
+    let length = '';
     if (this.state.prevTotal.toString().length > this.state.newInt.toString().length) { // sets the correct number of decimal places
       let ind = this.state.prevTotal
       .toString()
@@ -127,16 +172,16 @@ class Calculator extends React.Component {
       length = this.state.newInt.toString().length - ind - 1
     };
     if (currOp === 'plus') { // adding operation
-        prevTotal = this.state.prevTotal + this.state.newInt
+        prevTotal = parseFloat(this.state.prevTotal) + parseFloat(this.state.newInt);
       };
     if (currOp === 'minus') { // subtracting operation
-        prevTotal = this.state.prevTotal - this.state.newInt
+        prevTotal = parseFloat(this.state.prevTotal) - parseFloat(this.state.newInt);
     }
     if (currOp === 'multiply') { // multiplying operation
-        prevTotal = this.state.prevTotal * this.state.newInt
+        prevTotal = parseFloat(this.state.prevTotal) * parseFloat(this.state.newInt);
     }
     if (currOp === 'divide') { // dividing operation
-        prevTotal = this.state.prevTotal / this.state.newInt
+        prevTotal = parseFloat(this.state.prevTotal) / parseFloat(this.state.newInt);
     }
     if (/\./.test(prevTotal) === true) {
       prevTotal = prevTotal.toFixed(length);
@@ -150,11 +195,11 @@ class Calculator extends React.Component {
 
   clearPreview() { // resets all values
     this.setState({
-      text: 0,
+      text: '0',
       prev: '',
       operator: '',
       newInt: '',
-      prevTotal: 0,
+      prevTotal: '',
       firstOp: 'no',
       totaled: 'no'
     });
@@ -162,9 +207,9 @@ class Calculator extends React.Component {
 
   showTotal() { // handles operations and displays the outcome
     let currOp = this.state.operator;
-    let prevTotal = 0;
-    let text = 0;
-    let length = 0;
+    let prevTotal = '';
+    let text = '';
+    let length = '';
     if (this.state.prevTotal.toString().length > this.state.newInt.toString().length) { // sets the correct number of decimal places
       let ind = this.state.prevTotal
       .toString()
@@ -177,23 +222,39 @@ class Calculator extends React.Component {
       length = this.state.newInt.toString().length - ind - 1
     };
     if (currOp === 'plus') {
-      prevTotal = this.state.prevTotal + this.state.newInt;
-      text = this.state.prevTotal + this.state.newInt;
+      prevTotal = parseFloat(this.state.prevTotal) + parseFloat(this.state.newInt);
+      text = parseFloat(this.state.prevTotal) + parseFloat(this.state.newInt);
     }
     if (currOp === 'minus') {
-      prevTotal = this.state.prevTotal - this.state.newInt;
-      text = this.state.prevTotal - this.state.newInt;
+      prevTotal = parseFloat(this.state.prevTotal) - parseFloat(this.state.newInt);
+      text = parseFloat(this.state.prevTotal) - parseFloat(this.state.newInt);
     }
     if (currOp === 'multiply') {
-      prevTotal = this.state.prevTotal * this.state.newInt;
-      text = this.state.prevTotal * this.state.newInt;
+      prevTotal = parseFloat(this.state.prevTotal) * parseFloat(this.state.newInt);
+      text = parseFloat(this.state.prevTotal) * parseFloat(this.state.newInt);
     }
     if (currOp === 'divide') {
-      prevTotal = this.state.prevTotal / this.state.newInt;
-      text = this.state.prevTotal / this.state.newInt;
+      prevTotal = parseFloat(this.state.prevTotal) / parseFloat(this.state.newInt);
+      text = parseFloat(this.state.prevTotal) / parseFloat(this.state.newInt);
     }
     if (/\./.test(prevTotal) === true) {
-      text = text.toFixed(length);
+      let truthVal = 0;
+      let length = text.toString().length;
+      for (let i = length; i > 0; i--) {
+        let textLen = text.toString()[i - 1];
+        if (textLen === '0') {
+          truthVal *= 1 + 1;
+        } else if (/[1-9]/g.test(textLen) === true) {
+          truthVal += 1;
+        }
+        else if (textLen === '.') {
+          i = 0
+        }
+      }
+      if (truthVal > 4) {
+        truthVal = 4;
+      }
+      text = text.toFixed(truthVal);
     }
     this.setState({
       prevTotal: prevTotal,
@@ -245,33 +306,33 @@ class ButtonInput extends React.Component {
 
           <div className='btn-group'>
 
-            <button id='seven' onClick={() => this.props.handleNumChange('7')} className='btn btn-default btn-width'>7</button>
-            <button id='eight' onClick={() => this.props.handleNumChange('8')} className='btn btn-default btn-width'>8</button>
-            <button id='nine' onClick={() => this.props.handleNumChange('9')} className='btn btn-default btn-width'>9</button>
+            <button id='seven' onClick={() => this.props.handleNumChange('7')} className='btn btn-default btn-width 7'>7</button>
+            <button id='eight' onClick={() => this.props.handleNumChange('8')} className='btn btn-default btn-width 8'>8</button>
+            <button id='nine' onClick={() => this.props.handleNumChange('9')} className='btn btn-default btn-width 9'>9</button>
           
           </div>
 
           <div className='btn-group'>
 
-            <button id='four' onClick={() => this.props.handleNumChange('4')} className='btn btn-default btn-width'>4</button>
-            <button id='five' onClick={() => this.props.handleNumChange('5')} className='btn btn-default btn-width'>5</button>
-            <button id='six' onClick={() => this.props.handleNumChange('6')} className='btn btn-default btn-width'>6</button>
+            <button id='four' onClick={() => this.props.handleNumChange('4')} className='btn btn-default btn-width 4'>4</button>
+            <button id='five' onClick={() => this.props.handleNumChange('5')} className='btn btn-default btn-width 5'>5</button>
+            <button id='six' onClick={() => this.props.handleNumChange('6')} className='btn btn-default btn-width 6'>6</button>
           
           </div>
 
           <div className='btn-group'>
 
-            <button id='one' onClick={() => this.props.handleNumChange('1')} className='btn btn-default btn-width'>1</button>
-            <button id='two' onClick={() => this.props.handleNumChange('2')} className='btn btn-default btn-width'>2</button>
-            <button id='three' onClick={() => this.props.handleNumChange('3')} className='btn btn-default btn-width'>3</button>
+            <button id='one' onClick={() => this.props.handleNumChange('1')} className='btn btn-default btn-width 1'>1</button>
+            <button id='two' onClick={() => this.props.handleNumChange('2')} className='btn btn-default btn-width 2'>2</button>
+            <button id='three' onClick={() => this.props.handleNumChange('3')} className='btn btn-default btn-width 3'>3</button>
           
           </div>
 
           <div className='btn-group'>
 
             <button id='clear' onClick={this.props.clearPreview} className='btn btn-default btn-width'>Clear (Esc) </button>
-            <button id='zero' onClick={() => this.props.handleNumChange('0')} className='btn btn-default btn-width'>0</button>
-            <button id='decimal' onClick={() => this.props.handleDecChange('.')} className='btn btn-default btn-width'>.</button>
+            <button id='zero' onClick={() => this.props.handleNumChange('0')} className='btn btn-default btn-width 0'>0</button>
+            <button id='decimal' onClick={() => this.props.handleDecChange('.')} className='btn btn-default btn-width .'>.</button>
           
           </div>
 
@@ -292,7 +353,7 @@ class ButtonInput extends React.Component {
 
         <div className='col-xs-2'>
 
-          <button id='total' onClick={this.props.showTotal} className='btn btn-default btn-block btn-height-tall'>= (Enter)</button>
+          <button id='equals' onClick={this.props.showTotal} className='btn btn-default btn-block btn-height-tall'>= (Enter)</button>
         
         </div>
 
